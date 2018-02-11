2506083c1cadf3fe116c85424d78f832691cb4be
Niggeni <wantapi@web.de>
2018-02-11 13:08:23 +0100

    cpp file set up

---
+++ Cyvasse.cpp
@@ -0,0 +1,11 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <vector>
+#include <map>
+#include <math.h>
+using namespace std;
+int main(int, char**) {
+}
