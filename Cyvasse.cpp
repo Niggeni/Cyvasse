591b580d1e4f391d24c1d04bc9384456c2b4f39b
Nils <wantapi@web.de>
2018-02-12 08:38:41 +0100

    tidied up

--- Cyvasse.cpp
+++ Cyvasse.cpp
@@ -11,7 +11,7 @@ int main(int, char**) {
     SDL_Window *win = SDL_CreateWindow("Cyvasse", 100, 25, 8*128, 8*128, SDL_WINDOW_SHOWN);
     SDL_Surface *surf;
     surf = SDL_GetWindowSurface(win);
-    SDL_Surface *Pieces = IMG_Load("Sources/Dragon-head-silhouette/Pieces.png");
+    SDL_Surface *Pieces = IMG_Load("Sources/Pieces/Pieces.png");
     SDL_Surface *Board = IMG_Load("Sources/Board.png");
     SDL_Rect Piecerect = {x: 0, y: 0, w:5*128, h:2*128};
     SDL_Rect Boardrect = {x: 0, y: 0, w:8*128, h:8*128};
