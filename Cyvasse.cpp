c84961e5790a5f20e5a58672d87bda575ab8faa6
Nils <wantapi@web.de>
2018-02-16 16:10:12 +0100

    Spielfeld hinzugefügt, Figur implementiert

--- Cyvasse.cpp
+++ Cyvasse.cpp
@@ -8,6 +8,18 @@
 #include <math.h>
 #include "Pieces.h"
 using namespace std;
+class Spielfeld{
+    private:
+        SDL_Surface *surf;
+        SDL_Surface *Feld;
+        SDL_Rect Rect;
+        SDL_Rect source;
+        SDL_Rect dest;
+    public:
+        Spielfeld(SDL_Window*);
+        void aktualisieren();
+}
+
 class Figur{
     private:
         SDL_Surface *surf;
@@ -20,32 +32,27 @@ class Figur{
         int Team;
         int Typ;
     public:
-        Figur(int,int,SDL_Window*);
+        Figur(int,int,int,int,SDL_Window*);
         void aktualisieren();
-        void bewegen(int,int);
+        //void bewegen(int,int);
 };
-Figur::Figur(int xpos,int ypos,,int Teamvar,int Typvar,SDL_Window *win){
+Figur::Figur(int xpos,int ypos,int Teamvar,int Typvar,SDL_Window *win){
     x = xpos;
     y = ypos;
     Team = Teamvar;
     Typ = Typvar;
-    source = {x: 32*numx, y: 32*numy, w:32, h:32};
+    source = {x: 128*Typ, y: 128*Team, w:128, h:128};
     Pieces = IMG_Load("Sources/Pieces/Pieces.png");
     surf = SDL_GetWindowSurface(win);
 }
+void Figur::aktualisieren(){
+    Rect = {x:x,y:y,w:128,h:128};
+    SDL_BlitSurface(Pieces,&source,surf,&Rect);
+}
 int main(int, char**) {
-    std::cout << add(2,2) << '\n';
     SDL_Window *win = SDL_CreateWindow("Cyvasse", 0, 0, 1920 , 1080, SDL_WINDOW_SHOWN);
-    SDL_Surface *surf;
-    surf = SDL_GetWindowSurface(win);
-    SDL_Surface *Pieces = IMG_Load("Sources/Pieces/Pieces.png");
-    SDL_Surface *Board = IMG_Load("Sources/Board.png");
-    SDL_Rect Piecerect = {x: 0, y: 0, w:5*128, h:2*128};
-    SDL_Rect Boardrect = {x:0, y: 0, w:8*128, h:8*128};
-    SDL_Rect dest = {x: 300, y: 0, w:128, h:128};
-    SDL_Rect Boarddest =  {x:300,y:0,w:1920,h:1080};
-    SDL_BlitSurface(Board, &Boardrect, surf,&Boarddest);
-    SDL_BlitSurface(Pieces, &Piecerect, surf, &dest);
+    Figur Drache(2,2,0,1,win);
+    Drache.aktualisieren();
     SDL_UpdateWindowSurface(win);
     SDL_Delay(100000);
     return 1;
