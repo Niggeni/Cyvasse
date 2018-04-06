f2e16ea7216322d9605c1ee8df92d8f4a1fed055
niggeni <wantapi@web.de>
2018-04-06 13:09:54 +0200

    Dragon

--- Cyvasse.cpp
+++ Cyvasse.cpp
@@ -34,7 +34,7 @@ void Spielfeld::aktualisieren(/* arguments */) {
 void Spielfeld::getinput(const Uint8 *keystate){
 }
 class Figur{
-    private:
+    protected:
         SDL_Surface *surf;
         SDL_Surface *Pieces;
         SDL_Rect Rect;
@@ -45,21 +45,21 @@ class Figur{
         int Team;
         int Typ;
     public:
-        Figur(int,int,int,int,SDL_Window*);
+        Figur(int,int,int,SDL_Window*);
         void aktualisieren();
         void bewegen(int,int);
         //void bewegen(int,int);
 };
-Figur::Figur(int xpos,int ypos,int Teamvar,int Typvar,SDL_Window *win){
+Figur::Figur(int xpos,int ypos,int Teamvar,SDL_Window *win){
     x = xpos;
     y = ypos;
     Team = Teamvar;
-    Typ = Typvar;
     source = {x: 128*Typ, y: 128*Team, w:128, h:128};
     Pieces = IMG_Load("Sources/Pieces/Pieces.png");
     surf = SDL_GetWindowSurface(win);
 }
 void Figur::aktualisieren(){
+    source = {x: 128*Typ, y: 128*Team, w:128, h:128};
     Rect = {x:x*128+448,y:y*128+28,w:128,h:128};
     SDL_BlitSurface(Pieces,&source,surf,&Rect);
 }
@@ -67,23 +67,30 @@ void Figur::bewegen(int xpos, int ypos){
     x = xpos;
     y = ypos;
 }
-
+class Dragon :public Figur{
+    public:
+        Dragon(int xpos,int ypos,int Teamvar,SDL_Window *win): Figur(xpos,ypos,Teamvar,win){
+            Typ = 1;
+        };
+};
 int main(int, char**) {
     SDL_Window *win = SDL_CreateWindow("Cyvasse", 0, 0, 1920 , 1080, SDL_WINDOW_SHOWN);
     Spielfeld Brett(win);
-    vector<Figur*> Figuren;
-
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0;j<2;j++){
-            Figur *currfig = new Figur(i,j + j*(7-j),j,i,win);
-            Figuren.push_back(currfig);
-        }
-    }
+    // vector<Figur*> Figuren;
+    //
+    // for (int i = 0; i < 5; i++) {
+    //     for (int j = 0;j<2;j++){
+    //         Figur *currfig = new Figur(i,j + j*(7-j),j,i,win);
+    //         Figuren.push_back(currfig);
+    //     }
+    // }
+    Dragon Dragon_White(4,1,1,win);
     while (true){
+        Dragon_White.aktualisieren();
         Brett.aktualisieren();
-        for (int i = 0; i < int(Figuren.size()); i++) {
-            Figuren[i]->aktualisieren();
-        }
+        // for (int i = 0; i < int(Figuren.size()); i++) {
+        //     Figuren[i]->aktualisieren();
+        // }
         // SDL_PumpEvents();
         // if (SDL_GetMouseState(NULL, NULL) || SDL_BUTTON(SDL_BUTTON_LEFT)) {
         //     std::cout << (SDL_BUTTON(SDL_BUTTON_LEFT));
