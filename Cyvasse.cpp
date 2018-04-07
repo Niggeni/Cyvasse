4225b2934589757345bff747e7b95762ec584b6f
niggeni <wantapi@web.de>
2018-04-07 08:37:28 +0200

    Andere Figuren hinzugefügt

--- Cyvasse.cpp
+++ Cyvasse.cpp
@@ -59,38 +59,64 @@ Figur::Figur(int xpos,int ypos,int Teamvar,SDL_Window *win){
     surf = SDL_GetWindowSurface(win);
 }
 void Figur::aktualisieren(){
-    source = {x: 128*Typ, y: 128*Team, w:128, h:128};
     Rect = {x:x*128+448,y:y*128+28,w:128,h:128};
+    source = {x: 128*Typ, y: 128*Team, w:128, h:128};
     SDL_BlitSurface(Pieces,&source,surf,&Rect);
 }
 void Figur::bewegen(int xpos, int ypos){
     x = xpos;
     y = ypos;
 }
+class King :public Figur{
+public:
+    King(int xpos,int ypos,int Teamvar,SDL_Window *win): Figur(xpos,ypos,Teamvar,win){
+        Typ = 0;
+    };
+};
 class Dragon :public Figur{
     public:
         Dragon(int xpos,int ypos,int Teamvar,SDL_Window *win): Figur(xpos,ypos,Teamvar,win){
             Typ = 1;
         };
 };
+class Heavy_Cav :public Figur{
+    public:
+        Heavy_Cav(int xpos,int ypos,int Teamvar,SDL_Window *win): Figur(xpos,ypos,Teamvar,win){
+            Typ = 2;
+        };
+};
+class Light_Cav :public Figur{
+    public:
+        Light_Cav(int xpos,int ypos,int Teamvar,SDL_Window *win): Figur(xpos,ypos,Teamvar,win){
+            Typ = 3;
+        };
+};
+class Elephant :public Figur{
+    public:
+        Elephant(int xpos,int ypos,int Teamvar,SDL_Window *win): Figur(xpos,ypos,Teamvar,win){
+            Typ = 4;
+        };
+};
 int main(int, char**) {
     SDL_Window *win = SDL_CreateWindow("Cyvasse", 0, 0, 1920 , 1080, SDL_WINDOW_SHOWN);
     Spielfeld Brett(win);
-    // vector<Figur*> Figuren;
-    //
-    // for (int i = 0; i < 5; i++) {
-    //     for (int j = 0;j<2;j++){
-    //         Figur *currfig = new Figur(i,j + j*(7-j),j,i,win);
-    //         Figuren.push_back(currfig);
-    //     }
-    // }
-    Dragon Dragon_White(4,1,1,win);
+    vector<Figur*> Figuren;
+    for (int j = 0;j<2;j++){
+        Figuren.push_back(new King(0,j + j*(7-j),j,win));
+        Figuren.push_back(new Dragon(1,j + j*(7-j),j,win));
+        Figuren.push_back(new Heavy_Cav(2,j + j*(7-j),j,win));
+        Figuren.push_back(new Light_Cav(3,j + j*(7-j),j,win));
+        Figuren.push_back(new Elephant(4,j + j*(7-j),j,win));
+    }
+    //Dragon Dragon_White(4,1,0,win);
+    //King King_White(4,2,0,win);
     while (true){
-        Dragon_White.aktualisieren();
         Brett.aktualisieren();
-        // for (int i = 0; i < int(Figuren.size()); i++) {
-        //     Figuren[i]->aktualisieren();
-        // }
+        //Dragon_White.aktualisieren();
+        //King_White.aktualisieren();
+        for (int i = 0; i < int(Figuren.size()); i++) {
+         Figuren[i]->aktualisieren();
+        }
         // SDL_PumpEvents();
         // if (SDL_GetMouseState(NULL, NULL) || SDL_BUTTON(SDL_BUTTON_LEFT)) {
         //     std::cout << (SDL_BUTTON(SDL_BUTTON_LEFT));
